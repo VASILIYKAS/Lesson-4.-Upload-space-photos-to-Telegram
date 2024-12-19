628d4d552d9fa30f1e2d02e0f17796e0322dd04e
VASILIYKAS <yoyoynv@gmail.com>
2024-12-19 21:44:25 +0500

    Added automatic image publishing function

---
+++ post_telegram_bot.py
@@ -0,0 +1,54 @@
+import os
+import random
+import time
+import argparse
+from dotenv import load_dotenv
+from telegram import Bot
+
+
+def post_images(bot, channel_id, periodicity=14400):
+    images = []
+
+    for dirpath, directory, filenames in os.walk("images"):
+        for filename in filenames:
+            images.append(os.path.join(dirpath, filename))
+
+    while True:
+        if not images:
+            for dirpath, directory, filenames in os.walk("images"):
+                for filename in filenames:
+                    images.append(os.path.join(dirpath, filename))
+            random.shuffle(images)
+        
+        current_image = images.pop(0)
+        size_image = os.path.getsize(current_image)
+
+        if size_image < 20000000: 
+                bot.send_photo(chat_id=channel_id, photo=open(current_image, 'rb'))
+
+        time.sleep(periodicity)
+
+
+
+
+def main():
+    load_dotenv()
+
+    TOKEN = os.getenv('TG_TOKEN')
+    bot = Bot(token=TOKEN)
+    channel_id = '@Home_Gamer_tg'
+    periodicity = int(os.getenv('POST_PERIODICITY'))
+
+    parser = argparse.ArgumentParser(
+        description='Автоматическая публикация фотографий.'
+    )
+    parser.add_argument('--periodicity', type=str, default=14400,
+                        help='Частота публикаций в секундах. (По умолчанию 4 часа)'
+                        )
+
+    args = parser.parse_args()
+    post_images(bot, channel_id, args.periodicity)
+
+
+if __name__ == '__main__':
+    main()
