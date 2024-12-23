a0cbccaf5c7c512377013cfcd2b5c8b788fbf54d
VASILIYKAS <yoyoynv@gmail.com>
2024-12-23 21:34:50 +0500

    Added argument passing via the command line using argparse.

--- post_telegram_bot.py
+++ post_telegram_bot.py
@@ -1,5 +1,6 @@
 import os
 import random
+import argparse
 from dotenv import load_dotenv
 from telegram import Bot
 from image_handler import get_list_images, send_image
@@ -26,13 +27,28 @@ def post_image(bot, channel_id, image_name=None):
         random_image = random.choice(images)
         send_image(bot, channel_id, random_image)
 
+
 def main():
     load_dotenv()
 
     tg_token = os.environ['TG_TOKEN']
     bot = Bot(token=tg_token)
     channel_id = os.getenv('TG_CHAT_ID')
-    post_image(bot, channel_id)
+
+    parser = argparse.ArgumentParser(
+        description='Manual photo publishing.'
+    )
+    parser.add_argument(
+        '--image_name',
+        type=str,
+        default=None,
+        help='''Specify the filename including its extension. 
+        By default, a random one will be taken from the images folder.'''
+    )
+
+    args = parser.parse_args()
+    
+    post_image(bot, channel_id, args.image_name)
 
 
 if __name__ == '__main__':
